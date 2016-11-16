2dd8f14da85b68ffc970be5f6ff9b3bc325cb11b
Nishant Jain <nishantkumarajain@gmail.com>
2016-11-16 23:31:58 +0530

    Updated the cron to run every 30 minutes

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
+#!/usr/bin/env python
+
 import pyttsx
 import datetime
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 def get_time():
 	"""Returns the current time in normal english"""
+	
 	current_time = datetime.datetime.now().time()
 	if current_time.hour > 12:
-		return str(current_time.hour - 12) + ' ' + str(current_time.minute) + "PM"
+		return "It's " + str(current_time.hour - 12) + ' ' + str(current_time.minute) + "PM"
 	else:
-		return str(current_time.hour) + ' ' + str(current_time.minute) + "AM"
+		return "It's" + str(current_time.hour) + ' ' + str(current_time.minute) + "AM"
 
 def alarm():
 	engine = pyttsx.init()
 	engine.say(get_time())
 	engine.runAndWait()
 
-
-alarm()
+sched = BlockingScheduler()
+sched.add_job(alarm, 'cron', minute=30)
+sched.start()	
