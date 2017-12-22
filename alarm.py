9d1c1fcade5300b238bab52da8b8c9ec652cee6e
Nishant Jain <nishantkumarajain@gmail.com>
2017-12-22 11:41:37 +0530

    Added Comments for the packages used

--- alarm.py
+++ alarm.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
+# Python Offline Text to Speech
 import pyttsx
 import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 def get_time():
 	"""Returns the current time in normal english"""
-	
+
 	current_time = datetime.datetime.now().time()
 	if current_time.hour > 12:
 		return "It's " + str(current_time.hour - 12) + ' ' + str(current_time.minute) + "PM"
@@ -15,9 +16,10 @@ def get_time():
 
 def alarm():
 	engine = pyttsx.init()
-	engine.say(get_time())
+	time = get_time()
+	engine.say(time)
 	engine.runAndWait()
 
 sched = BlockingScheduler()
 sched.add_job(alarm, 'cron', minute=30)
-sched.start()	
+sched.start()
